6b34824e9c0f265887d06b9878a5e31b7f120a47
unknown <elie.bouabdallah.eba@gmail.com>
2023-06-06 15:30:06 +0300

    new updates css part2

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+let a = "elie";
+a=4+3;
+
+const PI = 3.1415;
+
+// console.log(PI);
+
+const person = {
+    name: "elie",
+    family:"Bou abdallah"
+}
+person["name"]="Dany";
+person.family="dodo";
+console.log(person);
